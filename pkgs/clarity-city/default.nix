72c5db7567bf0dce009841a80920d778844c8baa
Mark Sagi-Kazar <mark.sagikazar@gmail.com>
2023-10-04 11:55:51 +0200

    fix clarity city
    Signed-off-by: Mark Sagi-Kazar <mark.sagikazar@gmail.com>

--- pkgs/clarity-city/default.nix
+++ pkgs/clarity-city/default.nix
@@ -16,7 +16,7 @@ stdenv.mkDerivation rec {
     install -Dm644 OpenType/*.otf -t $out/share/fonts/opentype
     install -Dm644 Webfonts/EOT/*.eot -t $out/share/fonts/eot
     install -Dm644 Webfonts/WOFF/*.woff -t $out/share/fonts/woff
-    install -Dm644 Webfonts/WOFF2/*.woff2 -t $out/share/fonts/woff
+    install -Dm644 Webfonts/WOFF2/*.woff2 -t $out/share/fonts/woff2
   '';
 
   meta = with lib; {
