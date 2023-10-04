1a9a63107de84e68970a950c199dec01df05b54a
Mark Sagi-Kazar <mark.sagikazar@gmail.com>
2023-10-04 12:08:33 +0200

    fix clarity city
    Signed-off-by: Mark Sagi-Kazar <mark.sagikazar@gmail.com>

--- pkgs/clarity-city/default.nix
+++ pkgs/clarity-city/default.nix
@@ -12,15 +12,19 @@ stdenvNoCC.mkDerivation rec {
   };
 
   installPhase = ''
+    runHook preInstall
+
     install -Dm644 TrueType/*.ttf -t $out/share/fonts/truetype
     install -Dm644 OpenType/*.otf -t $out/share/fonts/opentype
     install -Dm644 Webfonts/EOT/*.eot -t $out/share/fonts/eot
     install -Dm644 Webfonts/WOFF/*.woff -t $out/share/fonts/woff
     install -Dm644 Webfonts/WOFF2/*.woff2 -t $out/share/fonts/woff2
+
+    runHook postInstall
   '';
 
   meta = with lib; {
-    description = "Clarity City font";
+    description = "Clarity City is an open source sans-serif typeface";
     homepage = "https://github.com/vmware/clarity-city";
     license = licenses.ofl;
     platforms = platforms.all;
